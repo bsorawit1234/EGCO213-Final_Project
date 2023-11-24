f411d8a5bcf570facd764aa9f39b7962da403d3e
teeThinnaphat <thinnaphat.phu@student.mahidol.edu>
2023-11-24 19:34:57 +0700

    add reenter password

--- src/main/java/Project3_6513122/RegisterFrame.java
+++ src/main/java/Project3_6513122/RegisterFrame.java
@@ -14,7 +14,7 @@ class RegisterFrame extends JFrame {
     private JLabel drawpane;
     private int framewidth = MyConstants.FRAMEWIDTH;
     private int frameheight = MyConstants.FRAMEHEIGHT;
-    private JPasswordField  passwordField;
+    private JPasswordField  passwordField, repasswordField;
     private JTextArea       usernameTextArea;
     private JButton         btn_submit;
     private ArrayList<User> UserList;
@@ -43,6 +43,7 @@ class RegisterFrame extends JFrame {
         JLabel label = new JLabel(Orange);
         JLabel username = new JLabel("Username : ");
         JLabel password = new JLabel("Password : ");
+        JLabel repassword = new JLabel("Re-enter Password : ");
         label.setBounds(300, 50,400 , 500);
         label.setIcon(Orange);
         label.setVisible(true);
@@ -52,8 +53,12 @@ class RegisterFrame extends JFrame {
         password.setVisible(true);
         password.setBounds(20, 200, 200, 100);
         password.setFont(new Font("Trend Sans One", Font.PLAIN, 25));
+        repassword.setVisible(true);
+        repassword.setBounds(20, 320, 300, 100);
+        repassword.setFont(new Font("Trend Sans One", Font.PLAIN, 25));
         label.add(username);
         label.add(password);
+        label.add(repassword);
 
         usernameTextArea = new JTextArea();
         usernameTextArea.setFont(new Font("SanSerif", Font.BOLD, 20));
@@ -67,20 +72,27 @@ class RegisterFrame extends JFrame {
         passwordField.setEchoChar('*'); // To hide the password characters
         drawpane.add(passwordField);
 
+        repasswordField = new JPasswordField();
+        repasswordField.setFont(new Font("SanSerif", Font.BOLD, 20));
+        repasswordField.setBounds(320, 450, 350, 30); // Adjust position and size as needed
+        repasswordField.setEchoChar('*'); // To hide the password characters
+        drawpane.add(repasswordField);
+
         btn_submit = new JButton("Submit");
         btn_submit.setFont(new Font("Trend Sans One", Font.PLAIN, 50));
-        btn_submit.setBounds(300, 400,400 , 50);
+        btn_submit.setBounds(300, 500,400 , 50);
         btn_submit.setForeground(Color.gray);
         btn_submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    register(usernameTextArea.getText(), new String(passwordField.getPassword()), UserList);
+                    register(usernameTextArea.getText(), new String(passwordField.getPassword()), new String(repasswordField.getPassword()), UserList);
                 } catch (MyException err) {
                     /*show JDialogue*/
                     JOptionPane.showMessageDialog(getParent(),err.getMessage());
                     usernameTextArea.setText("");
                     passwordField.setText("");
+                    repasswordField.setText("");
                 } catch (Exception err) {
                     System.err.println(err);
                 }
@@ -94,7 +106,7 @@ class RegisterFrame extends JFrame {
         validate();
     }
 
-    public void register(String username, String password, ArrayList<User> UserList) throws MyException {
+    public void register(String username, String password, String repassword, ArrayList<User> UserList) throws MyException {
         if(UserList != null) {
            for(User u : UserList) {
               if(u.getUsername().equals(username)) {
@@ -110,6 +122,9 @@ class RegisterFrame extends JFrame {
         }
         if(username.length() < 5 || password.length() < 5) {
             throw new MyException("Username and Password length must be greater than 5 characters.");
+        }
+        if(!password.equals(repassword)) {
+            throw new MyException("Password doesn't match.");
         }
 
         UserList.add(new User(username, password));
